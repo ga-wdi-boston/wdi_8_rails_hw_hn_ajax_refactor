792e7fafebfecb6db44391ebf9da064d7a20329a
brycelambert <brycelambert@gmail.com>
2013-11-07 09:33:45 -0500

    update db ENV

--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ set :public, 'public'
 enable :static
 
 set :database, {adapter: 'postgresql'}
-DataMapper.setup(:default, (ENV["DATABASE_URL"]))
+DataMapper.setup(:default, ENV["DATABASE_URL"])
 
 class Story < ActiveRecord::Base
   has_many :comments
